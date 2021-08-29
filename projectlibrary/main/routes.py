34478868914de66a31fd7564c48b8e9daaec8abe
averagewifiuser <averagewifiuser@gmail.com>
2021-08-29 01:49:04 +0100

    topic explore done

--- projectlibrary/main/routes.py
+++ projectlibrary/main/routes.py
@@ -6,9 +6,9 @@ from projectlibrary.models import *
 
 main = Blueprint('main', __name__)
 
-@main.route("/", methods=['POST', 'GET'], defaults={'page':1})
-@main.route("/home/<int:page>", methods=['GET', 'POST'])
-def home(page):
+
+@main.context_processor
+def inject_user():
     user = current_user
     student = None
     lecturer = None
@@ -17,9 +17,15 @@ def home(page):
         lecturer = Lecturers.query.filter_by(id=user.id).first()
         
         
-    
+    return dict(student=student, lecturer=lecturer)
+
+
+@main.route("/", methods=['POST', 'GET'], defaults={'page':1})
+@main.route("/home/<int:page>", methods=['GET', 'POST'])
+def home(page):
     #taking care of the reports
     reports = Reports.query.filter_by(published=True).paginate(page=page, per_page=4)
+    report_len = Reports
     
     next_url = url_for('main.home', page=reports.next_num) \
         if reports.has_next else None
@@ -51,49 +57,41 @@ def home(page):
                 
         else:
             flash('Wrong username/password combination', 'warning')
-    return render_template("Index.html", student=student, lecturer=lecturer, reports=reports, next_url=next_url, prev_url=prev_url)
+    return render_template("Index.html", reports=reports, next_url=next_url, prev_url=prev_url, report_len=report_len)
 
 
 @main.route("/abstract/<report_id>", methods=['GET', 'POST'])
-def abstract(report_id):
-    user = current_user
-    student = None
-    lecturer = None
-    if user.is_authenticated:
-        student = Students.query.filter_by(id=user.id).first()
-        lecturer = Lecturers.query.filter_by(id=user.id).first()
-        
-        
+def abstract(report_id):   
     report = Reports.query.filter_by(id=report_id).first()
-    return render_template('abstract.html', title='Report Abstract', report=report, student=student, lecturer=lecturer)
+    return render_template('abstract.html', title='Report Abstract', report=report)
 
 
 @main.route("/full-post/<report_id>", methods=['GET', 'POST'])
-def fullpost(report_id):
-    user = current_user
-    student = None
-    lecturer = None
-    if user.is_authenticated:
-        student = Students.query.filter_by(id=user.id).first()
-        lecturer = Lecturers.query.filter_by(id=user.id).first()
-        
-        
+def fullpost(report_id):    
     report = Reports.query.filter_by(id=report_id).first()
-    return render_template('fullpost.html', title='Report Full Post', report=report, student=student, lecturer=lecturer)
+    return render_template('fullpost.html', title='Report Full Post', report=report)
 
 
 
 @main.route("/colleges", methods=['POST', 'GET'])
-def colleges():
-    user = current_user
-    student = None
-    lecturer = None
-    if user.is_authenticated:
-        student = Students.query.filter_by(id=user.id).first()
-        lecturer = Lecturers.query.filter_by(id=user.id).first()
-        
-        
+def colleges():   
     return render_template('colleges.html', title='Colleges')
+
+
+@main.route("/explore/<topic>", methods=['POST', 'GET'], defaults={'page':1})
+@main.route("/explore/<topic>/<int:page>", methods=['POST', 'GET'])
+def topic_explore(topic, page):
+     #taking care of the reports
+    reports = Reports.query.filter_by(published=True, topic=topic).paginate(page=page, per_page=4)
+    report_len = Reports
+    
+    next_url = url_for('main.home', page=reports.next_num) \
+        if reports.has_next else None
+        
+    prev_url = url_for('main.home', page=reports.prev_num) \
+        if reports.has_prev else None
+        
+    return render_template('topic-explore.html', title=topic, reports=reports, prev_url=prev_url, next_url=next_url, report_len=report_len)
 
 
 @main.route("/logout")
